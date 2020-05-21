56e57803542778d631fe1f10189a4a64deb159df
gy991007 <johnyangao@163.com>
2020-05-21 19:50:17 +0800

    commit dist

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import App from '../pages/login/App.vue'
 
+
+
+
+const routerPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch(error => error)
+}
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: App
   }
 ]
 
--- front/vue.config.js
+++ front/vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pages: {
+    login: {
+      // entry for the page
+      entry: 'src/pages/login/main.js',
+      // output as dist/index.html
+      filename: 'login.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: '登入-学校综合管理系统'
+    }
+  },
+  assetsDir: 'static',
+  //publicPath: '/',
 }
