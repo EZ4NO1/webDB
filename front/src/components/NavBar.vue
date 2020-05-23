04a22ccff3bcadfd3879b27c0faccce6599a2881
alicemare <36297206+alicemare@users.noreply.github.com>
2020-05-23 09:01:28 +0800

    finish navbar.vue

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,54 +1,57 @@
 <template>
-  <nav>
+ <nav>
     <v-toolbar flat app color="blue" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title class="text-uppercase grey--text">
-      </v-toolbar-title>
+      <v-app-bar-nav-icon ></v-app-bar-nav-icon>
       <v-spacer>
       </v-spacer>
-      <v-btn flat color="blue" @click="logout()">
-        <span>Sign Out</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-navigation-drawer v-model="drawer" app>
-    <v-list shaped>
-      <v-subheader>Web-db</v-subheader>
-      <v-list-item-group v-model="item" color="primary">
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          router :to="item.route"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
+      <v-list-item v-for="(item, i) in items" :key="i">         
           <v-list-item-content>
+            <a :href=serverurl+item.route>
             <v-list-item-title v-text="item.text"></v-list-item-title>
+            </a>
           </v-list-item-content>
         </v-list-item>
-      </v-list-item-group>
-    </v-list>
-    </v-navigation-drawer>
+      <v-spacer>
+      </v-spacer>
+      <v-btn flat color="blue">
+        <span>登出</span>
+      </v-btn>
+    </v-toolbar>
   </nav>
-  
 </template>  
 
-<script>
+ 
+<style scoped>
+  a:hover, a:visited, a:link, a:active {
+    text-decoration: none;
+    color: white;
+}
+</style>
 
+<script>
   export default {
     data: () => ({
       drawer: true,
       item: 1,
+      serverurl :'http://localhost:8000',
       items: [
-        { text: 'Dashboard' , route: '/'},
-        { text: 'Campus' , route: '/campus'},
-      ]
+        { text: '学区管理',route: '/campus'},
+        { text: '专业管理',route: '/major'},
+        { text: '班级管理',route: '/class'},
+        { text: '学生管理',route: '/student' },
+        { text: '教师管理',route: '/teacher' },
+        { text: '课程管理',route: '/manage_course' },
+        { text: '学籍异动管理',route: '/unnormal_change' },
+      ],
     }),
-    methods: {
-        logout () {
-            this.$http.post('/api/logout')
-        }
+    methos:{
+      logout(){
+         this.$http.post('api/logout',JSON.stringify({})).then(function(res){
+                          window.location.href('/login')
+                        },function(res){
+                            alert(res.status)
+                        });       
       }
+    }
   }
 </script>
